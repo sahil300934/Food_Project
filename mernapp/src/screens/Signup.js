5e3e0aa23017e688cba6fa7e944409f81252f335
sahil300934 <101876067+sahil300934@users.noreply.github.com>
2023-07-10 13:17:47 +0530

    Update Signup.js

--- mernapp/src/screens/Signup.js
+++ mernapp/src/screens/Signup.js
@@ -7,7 +7,7 @@ export default function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(JSON.stringify({name: credentials.name, email: credentials.email, password: credentials.password, location: credentials.geolocation}))
-    const response = await fetch("http://localhost:5000/api/createuser", {
+    const response = await fetch("https://food-stores-edck.onrender.com/api/createuser", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
