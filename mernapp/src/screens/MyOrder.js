58ccb86a22b164d3dde5c0a9e8e0e5e2c7aac2fa
sahil300934 <101876067+sahil300934@users.noreply.github.com>
2023-07-10 13:17:25 +0530

    Update MyOrder.js

--- mernapp/src/screens/MyOrder.js
+++ mernapp/src/screens/MyOrder.js
@@ -6,7 +6,7 @@ export default function MyOrder() {
     const [orderData,setOrderData]=useState(""); 
     const fetchMyOrder=async()=>{
         console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:5000/api/myOrderData",{
+        await fetch("https://food-stores-edck.onrender.com/api/myOrderData",{
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
