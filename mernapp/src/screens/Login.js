3227ce840907d1d215fb6e1f916ff97ec88768c1
sahil300934 <101876067+sahil300934@users.noreply.github.com>
2023-07-10 13:17:04 +0530

    Update Login.js

--- mernapp/src/screens/Login.js
+++ mernapp/src/screens/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(JSON.stringify({ email: credentials.email, password: credentials.password }))
-    const response = await fetch("http://localhost:5000/api/loginuser", {
+    const response = await fetch("https://food-stores-edck.onrender.com/api/loginuser", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
