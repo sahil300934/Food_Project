b45ec8c689c5e76349218c9c7ed167323a6e4ece
sahil300934 <101876067+sahil300934@users.noreply.github.com>
2023-07-10 13:16:20 +0530

    Update Home.js

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
   const [foodItem, setFoodItem] = useState([]);
 
   const loadData = async () => {
-    let response = await fetch("http://localhost:5000/api/foodData", {
+    let response = await fetch("https://food-stores-edck.onrender.com/api/foodData", {
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
